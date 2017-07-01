f3f7d2ec99425859a47051d969b538477b50f164
robin.faury <robin.faury@gmail.com>
2017-07-01 10:22:42 +0400

    Initial commit

---
+++ Source/BID/BIDGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BIDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BID_API ABIDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
